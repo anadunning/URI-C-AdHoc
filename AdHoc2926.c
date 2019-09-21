53b6361ab2dffe5dc2c17e44c165b415de3b56bb
anadunning <anakpedrosa@hotmail.com>
2019-09-20 23:16:29 -0300

    So This is Christmas!

---
+++ AdHoc2926.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, j;
+
+    scanf("%d", &j);
+
+    printf("Ent");
+
+    for (i = 0; i < j; i++){
+        printf("a");
+    }
+
+    printf("o eh N");
+
+    for (i = 0; i < j; i++){
+        printf("a");
+    }
+
+    printf("t");
+
+    for (i = 0; i < j; i++){
+        printf("a");
+    }
+
+    printf("l!\n");
+
+    return 0;
+}
